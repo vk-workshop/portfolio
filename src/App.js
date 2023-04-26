31f6fc6a4861a742f0ef5f8ee50159289109264e
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-26 22:08:16 +0200

    Update App.js
    added icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./styles/main.css";
-import sun from "./img/icons/sun.svg"
-import moon from "./img/icons/moon.svg"
+import sun from "./img/icons/sun.svg";
+import moon from "./img/icons/moon.svg";
+import instagram from "./img/icons/instagram.svg";
+import twitter from "./img/icons/twitter.svg";
+import gitHub from "./img/icons/gitHub.svg";
+import linkedIn from "./img/icons/linkedIn.svg";
 
 
 function App() {
@@ -55,11 +59,10 @@ function App() {
         <div className="container">
             <div className="footer__wrapper">
                 <ul className="social">
-                    <li className="social__item"><a href="#!"><img src="./img/icons/vk.svg" alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src="./img/icons/instagram.svg" alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src="./img/icons/twitter.svg" alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src="./img/icons/gitHub.svg" alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src="./img/icons/linkedIn.svg" alt="Link" /></a></li>
+                    <li className="social__item"><a href="https://instagram.com/another_shine?igshid=YmMyMTA2M2Y="><img src={instagram} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
+                    <li className="social__item"><a href="https://github.com/anotherShine"><img src={gitHub} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
                 </ul>
                 <div className="copyright">
                     <p>© 2022 frontend-dev.com</p>
