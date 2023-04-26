90cc2132854fe0aa3974e9c90b7f04aead2dfef2
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-26 23:26:17 +0200

    replaced
    replaced  <Header /> and main section to Home

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import "./styles/main.css";
 import Navbar from "./components/navbar/Navbar";
-import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-
-
-
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-  
-      <Header />
 
+      <Home />
+      
       <Footer />
-
-      <main className="section">
-      <div className="container">
-        <ul className="content-list">
-          <li className="content-list__item">
-            <h2 className="title-2">Frontend</h2>
-            <p>
-              JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM,
-              BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
-            </p>
-          </li>
-        </ul>
-      </div>
-    </main>
-
- 
     </div>
   );
 }
