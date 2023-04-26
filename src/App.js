fe5c4f40e680619aa1ffe30ca6ac126573b36161
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-26 22:21:32 +0200

    Update App.js
    imported Navbar to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/main.css";
-import sun from "./img/icons/sun.svg";
-import moon from "./img/icons/moon.svg";
+import Navbar from "./components/navbar/Navbar";
+
 import instagram from "./img/icons/instagram.svg";
 import twitter from "./img/icons/twitter.svg";
 import gitHub from "./img/icons/gitHub.svg";
@@ -10,24 +10,8 @@ import linkedIn from "./img/icons/linkedIn.svg";
 function App() {
   return (
     <div className="App">
-          <nav className="nav">
-        <div className="container">
-            <div className="nav-row">
-                <a href="./index.html" className="logo"><strong>My</strong> portfolio</a>
-
-                <button className="dark-mode-btn">
-                    <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-                    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-                </button>
-
-                <ul className="nav-list">
-                    <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                    <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                    <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+      <Navbar />
+  
 
     <header className="header">
         <div className="header__wrapper">
