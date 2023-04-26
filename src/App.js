ea99110b51e422fda5cfacdcb952489dbb278ba5
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-26 23:10:09 +0200

    Update App.js
    imported Footer to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./styles/main.css";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 
-import instagram from "./img/icons/instagram.svg";
-import twitter from "./img/icons/twitter.svg";
-import gitHub from "./img/icons/gitHub.svg";
-import linkedIn from "./img/icons/linkedIn.svg";
 
 
 function App() {
@@ -16,37 +13,23 @@ function App() {
   
       <Header />
 
-  
+      <Footer />
 
-    <main className="section">
-        <div className="container">
-
-                <ul className="content-list">
-                    <li className="content-list__item">
-                        <h2 className="title-2">Frontend</h2>
-                        <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
-                    </li>
-                </ul>
-
-        </div>
+      <main className="section">
+      <div className="container">
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Frontend</h2>
+            <p>
+              JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM,
+              BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+            </p>
+          </li>
+        </ul>
+      </div>
     </main>
 
-    <footer className="footer">
-        <div className="container">
-            <div className="footer__wrapper">
-                <ul className="social">
-                    <li className="social__item"><a href="https://instagram.com/another_shine?igshid=YmMyMTA2M2Y="><img src={instagram} alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
-                    <li className="social__item"><a href="https://github.com/anotherShine"><img src={gitHub} alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
-                </ul>
-                <div className="copyright">
-                    <p>© 2023 frontend-dev.com</p>
-                </div>
-            </div>
-        </div>
-    </footer>
-
+ 
     </div>
   );
 }
