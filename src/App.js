74c835f239430355e96b8a51dbd91df79c42f950
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-26 22:18:34 +0200

    replaced
    replaced nav to navbar file

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
                     <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
                 </ul>
                 <div className="copyright">
-                    <p>© 2022 frontend-dev.com</p>
+                    <p>© 2023 frontend-dev.com</p>
                 </div>
             </div>
         </div>
