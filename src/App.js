fdd8b03e4994270ba6ac99e2b2d291bda380c56d
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-26 23:43:57 +0200

    imported
    imported jpg to Projects

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import "./styles/main.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
-      <Home />
-      
+      {/* <Home /> */}
+      <Projects />
       <Footer />
     </div>
   );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,9 @@
-
+import project1 from "../img/projects/01.jpg"
+import project2 from "../img/projects/02.jpg"
+import project3 from "../img/projects/03.jpg"
+import project4 from "../img/projects/04.jpg"
+import project5 from "../img/projects/05.jpg"
+import project6 from "../img/projects/06.jpg"
 
 
 const Projects = () => {
@@ -9,33 +14,33 @@ const Projects = () => {
             <ul className="projects">
                 <li className="project">
                     <a href="./project-page.html">
-                        <img src="./img/projects/01.jpg" alt="Project img" className="project__img" />
+                        <img src={project1} alt="Project img" className="project__img" />
                         <h3 className="project__title">Gaming streaming portal</h3>
                     </a>
                 </li>
                 <li className="project">
                     <a href="./project-page.html">
-                        <img src="./img/projects/02.jpg" alt="Project img" className="project__img" />
+                        <img src={project2} alt="Project img" className="project__img" />
                         <h3 className="project__title">Video service</h3>
                     </a>
                 </li>
                 <li className="project">
                     <a href="./project-page.html">
-                        <img src="./img/projects/03.jpg" alt="Project img" className="project__img" />
+                        <img src={project3} alt="Project img" className="project__img" />
                         <h3 className="project__title">Video portal</h3>
                     </a>
                 </li>
 
                 <li className="project">
-                    <img src="./img/projects/04.jpg" alt="Project img" className="project__img" />
+                    <img src={project4} alt="Project img" className="project__img" />
                     <h3 className="project__title">Dating app</h3>
                 </li>
                 <li className="project">
-                    <img src="./img/projects/05.jpg" alt="Project img" className="project__img" />
+                    <img src={project5} alt="Project img" className="project__img" />
                     <h3 className="project__title">Landing</h3>
                 </li>
                 <li class="project">
-                    <img src="./img/projects/06.jpg" alt="Project img" className="project__img" />
+                    <img src={project6} alt="Project img" className="project__img" />
                     <h3 className="project__title">Gaming community</h3>
                 </li>
 
