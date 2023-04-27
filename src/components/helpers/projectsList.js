1fe2eea62f1f0e9120100084e54c55e21f55efb7
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-27 13:30:33 +0200

    imported
    imported img

--- src/components/helpers/projectsList.js
+++ src/components/helpers/projectsList.js
@@ -1,16 +1,9 @@
 import project01 from "../../img/projects/01.jpg";
+import project02 from "../../img/projects/dino-game.jpg";
 import project01Big from "../../img/projects/01-big.jpg";
 import { v1 } from "uuid"
 
 export const projectsList = [
-    {
-        id: v1(),
-        title: 'Dino Game',
-        skills: 'JS, CSS, HTML',
-        img: project01,
-        imgBig: project01Big,
-        gitHubLink: 'https://github.com/anotherShine'
-    },
     {
         id: v1(),
         title: 'Social Network',
@@ -23,6 +16,13 @@ export const projectsList = [
         title: 'Memory Game',
         skills: 'JS, CSS, HTML',
         img: project01,
+        gitHubLink: 'https://github.com/anotherShine'
+    },
+    {
+        id: v1(),
+        title: 'Dino Game',
+        skills: 'JS, CSS, HTML',
+        img: project02,
         gitHubLink: 'https://github.com/anotherShine'
     },
     {
