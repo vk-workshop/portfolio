41f70caecfea365f8b9ddfa8ed36c8f73b7de7e1
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-27 11:35:21 +0200

    Update Projects.js
    replaced to Project.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,4 @@
-import project1 from "../img/projects/01.jpg"
+
 import project2 from "../img/projects/02.jpg"
 import project3 from "../img/projects/03.jpg"
 import project4 from "../img/projects/04.jpg"
@@ -12,12 +12,7 @@ const Projects = () => {
         <div className="container">
             <h2 className="title-1">Projects</h2>
             <ul className="projects">
-                <li className="project">
-                    <a href="./project-page.html">
-                        <img src={project1} alt="Project img" className="project__img" />
-                        <h3 className="project__title">Gaming streaming portal</h3>
-                    </a>
-                </li>
+               
                 <li className="project">
                     <a href="./project-page.html">
                         <img src={project2} alt="Project img" className="project__img" />
