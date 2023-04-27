ff2abdff4985480efdbb2a740707dbfc53e04092
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-27 23:06:05 +0200

    Update App.js
    added ScrollToTop in App

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import Projects from "./pages/Projects";
 import Project from "./pages/Project";
 import Contacts from "./pages/Contacts";
 
+import ScrollToTop from "./utils/scrollToTop"
+
 function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
