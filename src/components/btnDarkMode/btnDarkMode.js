2ae44d506938df560628f2610a013d405bb061d6
Vova <90705859+anotherShine@users.noreply.github.com>
2023-05-01 14:42:23 +0200

    Update btnDarkMode.js
    added btnRef.current.classList.add or  remove

--- src/components/btnDarkMode/btnDarkMode.js
+++ src/components/btnDarkMode/btnDarkMode.js
@@ -12,9 +12,11 @@ const BtnDarkMode = () => {
 
     useEffect(() => {
      if (darkMode === 'light') {
-        document.body.classList.add('dark')
+        document.body.classList.add('dark');
+        btnRef.current.classList.add('dark-mode-btn--active')
      } else {
-        document.body.classList.remove('dark')
+        document.body.classList.remove('dark');
+        btnRef.current.classList.remove('dark-mode-btn--active')
      }
     }, [darkMode])
 
@@ -22,7 +24,7 @@ const BtnDarkMode = () => {
         return currentValue === 'light'? 'dark' : 'light'
     })}
     return ( 
-        <button onClick={toggleDarkMode} className="dark-mode-btn">
+        <button ref={btnRef} onClick={toggleDarkMode} className="dark-mode-btn">
         <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
         <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
       </button>
