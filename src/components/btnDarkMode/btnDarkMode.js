6d54e8d6bb19339455d6a50d0ba70ee2a6f7246b
Vova <90705859+anotherShine@users.noreply.github.com>
2023-05-01 13:57:37 +0200

    Update btnDarkMode.js
    creasted toggleDarkMode function

--- src/components/btnDarkMode/btnDarkMode.js
+++ src/components/btnDarkMode/btnDarkMode.js
@@ -1,12 +1,16 @@
 
+
 import sun from "./sun.svg";
 import moon from "./moon.svg";
 import "./btnDarkMode.css"
 
 
 const BtnDarkMode = () => {
+    const toggleDarkMode = () => {
+        window.alert("ddd")
+    }
     return ( 
-        <button className="dark-mode-btn">
+        <button onClick={toggleDarkMode} className="dark-mode-btn">
         <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
         <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
       </button>
