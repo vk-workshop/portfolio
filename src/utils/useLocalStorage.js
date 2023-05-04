d5b538f0ae1ddbfc81a8fd828f393c42ab12ed12
Vova <90705859+anotherShine@users.noreply.github.com>
2023-05-04 20:16:49 +0200

    Update useLocalStorage.js
    added useEffect

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -10,5 +10,12 @@ function getStorageValue (key, defaultValue) {
 export const useLocalStorage = (key, defaultValue) => {
     const [value, setValue] = useState(() => {
         return getStorageValue(key, defaultValue)
-    })
+    });
+
+    useEffect(() => {
+        //storing input name
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
+    
+    return [value, setValue]
 };
