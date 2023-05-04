58d35a56cd71c9c3a1f384a2df1673a329b1906b
Vova <90705859+anotherShine@users.noreply.github.com>
2023-05-04 23:42:31 +0200

    Update btnDarkMode.js
    delete unused useRef

--- src/components/btnDarkMode/btnDarkMode.js
+++ src/components/btnDarkMode/btnDarkMode.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useLocalStorage } from "../../utils/useLocalStorage";
 import { detectDarkMode } from "../../utils/detectDarkMode";
 import sun from "./sun.svg";
@@ -7,8 +7,6 @@ import "./btnDarkMode.css";
 
 const BtnDarkMode = () => {
   const [darkMode, setDarkMode] = useLocalStorage("darkMode", detectDarkMode());
-
-  const btnRef = useRef(null);
 
   useEffect(() => {
     if (darkMode === "light") {
@@ -38,12 +36,9 @@ const BtnDarkMode = () => {
 
   const btnActive = "dark-mode-btn dark-mode-btn--active";
   const btnNormal = "dark-mode-btn";
-  
+
   return (
-    <button 
-    ref={btnRef} 
-    onClick={toggleDarkMode} 
-    className={darkMode === "light"? btnActive: btnNormal}>
+    <button onClick={toggleDarkMode} className={darkMode === "light"? btnActive: btnNormal}>
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
