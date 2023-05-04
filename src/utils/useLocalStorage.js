819f46edf6d72183987f9dd15299aa5e56df41c2
Vova <90705859+anotherShine@users.noreply.github.com>
2023-05-04 20:13:56 +0200

    Update useLocalStorage.js
    created useLocalStorage function

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -5,4 +5,10 @@ function getStorageValue (key, defaultValue) {
     const saved = localStorage.getItem(key);
     const initial = JSON.parse(saved);
     return initial || defaultValue;
-}
+};
+
+export const useLocalStorage = (key, defaultValue) => {
+    const [value, setValue] = useState(() => {
+        return getStorageValue(key, defaultValue)
+    })
+};
