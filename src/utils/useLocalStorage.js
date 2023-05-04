ea1cd925a8a9146f19573b8cd42a68db227ae87e
Vova <90705859+anotherShine@users.noreply.github.com>
2023-05-04 20:09:41 +0200

    Update useLocalStorage.js
    created getStorageValue function

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -1,2 +1,8 @@
 import { useState, useEffect } from "react";
 
+function getStorageValue (key, defaultValue) {
+    //getting storage value
+    const saved = localStorage.getItem(key);
+    const initial = JSON.parse(saved);
+    return initial || defaultValue;
+}
