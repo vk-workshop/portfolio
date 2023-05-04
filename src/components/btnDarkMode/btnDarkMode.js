28692a73c6f53821588d08a2bd68ca472971582b
Vova <90705859+anotherShine@users.noreply.github.com>
2023-05-04 23:41:14 +0200

    Update btnDarkMode.js
    added btnActive and btnNormal

--- src/components/btnDarkMode/btnDarkMode.js
+++ src/components/btnDarkMode/btnDarkMode.js
@@ -13,10 +13,10 @@ const BtnDarkMode = () => {
   useEffect(() => {
     if (darkMode === "light") {
       document.body.classList.add("dark");
-      btnRef.current.classList.add("dark-mode-btn--active");
+    //   btnRef.current.classList.add("dark-mode-btn--active");
     } else {
       document.body.classList.remove("dark");
-      btnRef.current.classList.remove("dark-mode-btn--active");
+    //   btnRef.current.classList.remove("dark-mode-btn--active");
     }
   }, [darkMode]);
 
@@ -28,15 +28,22 @@ const BtnDarkMode = () => {
         const newColorScheme = event.matches ? "dark" : "light";
         setDarkMode(newColorScheme);
       });
-  });
+  },);
 
   const toggleDarkMode = () => {
     setDarkMode((currentValue) => {
       return currentValue === "light" ? "dark" : "light";
     });
   };
+
+  const btnActive = "dark-mode-btn dark-mode-btn--active";
+  const btnNormal = "dark-mode-btn";
+  
   return (
-    <button ref={btnRef} onClick={toggleDarkMode} className="dark-mode-btn">
+    <button 
+    ref={btnRef} 
+    onClick={toggleDarkMode} 
+    className={darkMode === "light"? btnActive: btnNormal}>
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
