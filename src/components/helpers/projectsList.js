67a4cdac7c1bddb24cc6507898ed248f13abe86d
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-07-28 19:22:21 +0200

    add
    add admin page project

--- src/components/helpers/projectsList.js
+++ src/components/helpers/projectsList.js
@@ -8,6 +8,7 @@ import project021 from "../../img/projects/dino-2.png";
 // import project041 from "../../img/projects/social-network-Big.png";
 import project05 from "../../img/projects/st-1.png";
 import project051 from "../../img/projects/store-2.png";
+import project052 from "../../img/projects/admin-1.png";
 import project6 from "../../img/projects/sum-1.png";
 import project61 from "../../img/projects/sum-4.png";
 import { v1 } from "uuid";
@@ -16,12 +17,19 @@ export const projectsList = [
   {
     id: v1(),
     title: "Store + Admin Dashboard",
-    skills: "HTML, CSS, Tailwind CSS, React.js, TypeScript, Next.js, Headless UI, PostCSS, Stripe, Zustand, Axios, Recharts, Zod, Next, Cloudinary, Next Themes",
+    title2: "eCommerce Store",
+    title3: "Admin Dashboard",
+    skills: "HTML, CSS, Tailwind CSS, React.js, TypeScript, Next.js, Headless UI, PostCSS",
+    skills2: "Prisma, Next.js, Stripe, Zustand, Axios, Recharts, Zod, Next Cloudinary, Next Themes",
     link: "https://ecommerce-store-zeta-sable.vercel.app/",
+    link2: "https://ecommerce-admin-phi-seven.vercel.app//",
     img: project05,
     imgBig: project051,
+    img2: project052,
     gitHubLink:
       "https://github.com/VolodymyrKolisnichenko/ecommerce-store/blob/master/README.md",
+    gitHubLink2:
+      "https://github.com/VolodymyrKolisnichenko/ecommerce-admin/blob/main/README.md",
   },
   {
     id: v1(),
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -6,29 +6,70 @@ const Project = () => {
   const { id } = useParams();
   const project = projectsList[id];
 
-  return (
-    <main className="section">
-      <div className="container">
-        <div className="project-details">
-          <h1 className="title-1">{project.title}</h1>
+  if (Number(id) === 0) {
+    return (
+      <main className="section">
+        <div className="container">
+          <div className="project-details">
+            <h1 className="title-1">{project.title2}</h1>
 
-          <a href={project.link} target="_blank" rel="noreferrer">
-            <img
-              src={project.imgBig}
-              alt={project.title}
-              className="project-details__cover"
-            />
-          </a>
+            <a href={project.link} target="_blank" rel="noreferrer">
+              <img
+                src={project.imgBig}
+                alt={project.title}
+                className="project-details__cover"
+              />
+            </a>
 
-          <div className="project-details__desc">
-            <p>{project.skills}</p>
+            <div className="project-details__desc">
+              <p>{project.skills}</p>
+            </div>
+
+            <BtnGitHub link={project.gitHubLink} />
+            <h1 className="title-1">{project.title3}</h1>
+
+            <a href={project.link2} target="_blank" rel="noreferrer">
+              <img
+                src={project.img2}
+                alt={project.title3}
+                className="project-details__cover"
+              />
+            </a>
+
+            <div className="project-details__desc">
+              <p>{project.skills2}</p>
+            </div>
+
+            <BtnGitHub link={project.gitHubLink2} />
           </div>
-
-          <BtnGitHub link={project.gitHubLink} />
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  } else {
+    return (
+      <main className="section">
+        <div className="container">
+          <div className="project-details">
+            <h1 className="title-1">{project.title}</h1>
+
+            <a href={project.link} target="_blank" rel="noreferrer">
+              <img
+                src={project.imgBig}
+                alt={project.title}
+                className="project-details__cover"
+              />
+            </a>
+
+            <div className="project-details__desc">
+              <p>{project.skills}</p>
+            </div>
+
+            <BtnGitHub link={project.gitHubLink} />
+          </div>
+        </div>
+      </main>
+    );
+  }
 };
 
 export default Project;
