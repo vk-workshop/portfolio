1351c9115a227e156641ed6f11b5844acdf0893a
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-08-08 22:35:52 +0200

    slide_left
    add slide_left class

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="header__wrapper">
             <h1 className="header__title">
                 <strong>Hi, my name is <em>Volodymyr</em></strong><br/>
-                a frontend developer
+                a frontend <span className="slide_left">developer</span>
             </h1>
             <div className="header__text">
                 <p>with passion for learning and creating.</p>
