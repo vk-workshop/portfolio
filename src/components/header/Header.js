c6c8e607fff5a02d0b213cac5f3919cccf82310c
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-08-10 10:13:00 +0200

    new color
    add new color

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,27 @@
 import "./Header.css";
 
 const Header = () => {
-    const cvURL = process.env.PUBLIC_URL + '/Volodymyr_Kolisnichenko_CV.pdf';
-    return ( 
-        <>
-        <header className="header">
+  const cvURL = process.env.PUBLIC_URL + "/Volodymyr_Kolisnichenko_CV.pdf";
+  return (
+    <>
+      <header className="header">
         <div className="header__wrapper">
-            <h1 className="header__title">
-                <strong>Hi, my name is <em>Volodymyr</em></strong><br/>
-                a frontend <span className="slide_left">developer</span>
-            </h1>
-            <div className="header__text">
-                <p>with passion for learning and creating.</p>
-            </div>
-            <a href={cvURL} download="" className="btn">Download CV</a>
+          <h1 className="header__title">
+            <strong>
+              Hi, my name is <em>Volodymyr</em>
+            </strong>
+            <br />a <span className="slide__left">frontend developer</span>
+          </h1>
+          <div className="header__text">
+            <p>with passion for learning and creating.</p>
+          </div>
+          <a href={cvURL} download="" className="btn">
+            Download CV
+          </a>
         </div>
-    </header>
+      </header>
     </>
-     );
-}
- 
+  );
+};
+
 export default Header;
