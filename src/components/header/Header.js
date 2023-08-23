2f8986b95d58734288a1441d73c317f009b697e7
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-08-23 16:19:48 +0200

    textAnimation
    created textAnimation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,30 +1,48 @@
 import "./Header.css";
 
+import { motion } from "framer-motion";
+
+
+const textAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0
+  },
+  visible: custom => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2}
+  })
+}
+
 const Header = () => {
   const cvURL = process.env.PUBLIC_URL + "/Volodymyr_Kolisnichenko_CV.pdf";
   return (
     <>
-      <header className="header">
+      <motion.header 
+      initial="hidden"
+      whileInView="visible"
+      className="header">
         <div className="header__wrapper">
           <h1 className="header__title">
-            <p className="slide__right">
+            <motion.p custom={1} variants={textAnimation} >
               <strong>
                 Hi, my name is 
               </strong>
-            </p>
-            <strong><em>Volodymyr</em></strong>
-            <br />a <span>frontend developer</span>
+            </motion.p>
+            <motion.p custom={2} variants={textAnimation}><strong><em>Volodymyr</em></strong></motion.p> 
+            <motion.p custom={6} variants={textAnimation}> a <span>frontend developer</span></motion.p>
           </h1>
           <div className="header__text">
-            <p className="slide__left">
+            <motion.p custom={4} variants={textAnimation}>
               with passion for <span> learning and creating </span>.
-            </p>
+            </motion.p>
           </div>
           <a href={cvURL} download="" className="btn">
             Download CV
           </a>
         </div>
-      </header>
+      </motion.header>
     </>
   );
 };
