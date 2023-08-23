c29d44bd356a97c9b5dd7dfde06cf0adc29fe347
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-08-23 17:47:55 +0200

    textAnimation
    add textAnimation

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,19 +1,15 @@
 import { NavLink } from "react-router-dom";
 import "./Project.css";
 
-import { forwardRef } from 'react';
-
-
-
-const Project = forwardRef(({ img, title, index }, ref) => {
+const Project = ({ img, title, index }) => {
   return (
-    <NavLink to={`/project/${index}`} ref={ref}>
+    <NavLink to={`/project/${index}`}>
       <li className="project">
         <img src={img} alt="Project img" className="project__img" />
         <h3 className="project__title">{title}</h3>
       </li>
     </NavLink>
   );
-});
+};
 
 export default Project;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,40 @@ import Header from "../components/header/Header";
 
 import { motion } from "framer-motion";
 
+const textAnimation = {
+  hidden: {
+    y: 100,
+    opacity: 0
+  },
+  visible: custom => ({
+    y: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2}
+  })
+}
+
 const Home = () => {
   return (
     <>
       <Header />
 
-      <main className="section">
+      <motion.main 
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.7 }}
+      className="section">
         <div className="container">
           <ul className="content-list">
             <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>
+              <motion.h2 custom={1} variants={textAnimation} className="title-2">Frontend</motion.h2>
+              <motion.p custom={2} variants={textAnimation}>
                 JavaScript, TypeScript, ReactJS, NextJS, Redux, Valtio, HTML, CSS, SCSS,
                 BootStrap, MaterialUI, TailwindCSS, Git
-              </p>
+              </motion.p>
             </li>
           </ul>
         </div>
-      </main>
+      </motion.main>
     </>
   );
 };
